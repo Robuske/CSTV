96ecaffcb34c6e3fe54abb6b78ff26ea3bae7a5b
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2024-08-27 23:48:21 -0300

    Initial Commit

---
+++ CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by Rodrigo Cardoso Buske on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSTV/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CSTV
+//
+//  Created by Rodrigo Cardoso Buske on 27/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
